32ef15fed6546b24a3750c874cb9095ff0315dd6
Larissa Moro <54962550+LariMoro20@users.noreply.github.com>
2022-06-20 00:44:05 -0300

    [UPDATES] Add functions and code improvements

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,64 +1,23 @@
+//Variaveis globais
+let calculation = ''
+let has_operation = false
+
+const result_area = document.getElementById(`ResultArea`); // Area de exibição dos calculos
+const clear_button = document.getElementById(`ClearBtn`); //Botão de limpar
+
 //Resolução da imagem
 var largura = document.getElementById("calculator").width;
 var altura = document.getElementById("calculator").height;
-let result = 0;
-let num1 = 0;
-let num2 = 0;
-//Definição das coordenadas utilizando as devidas proporções
-//larg = 500(x)
-//alt = 676(y)
-//largcord = x*100/larg
-//altcord = y*100/alt
 
-function cal(...num) {
-    return num.join("");
-}
-const action_coord = [
-    {
-        1: largura * 0.73,
-        2: altura * 0.2875,
-        3: largura * 0.91,
-        4: altura * 0.424,
-        val: "/",
-    },
-    {
-        1: largura * 0.73,
-        2: altura * 0.452,
-        3: largura * 0.91,
-        4: altura * 0.58,
-        val: "x",
-    },
-    {
-        1: largura * 0.73,
-        2: altura * 0.61,
-        3: largura * 0.98,
-        4: altura * 0.74,
-        val: "-",
-    },
-    {
-        1: largura * 0.3,
-        2: altura * 0.77,
-        3: largura * 0.48,
-        4: altura * 0.9,
-        val: ".",
-    },
-    {
-        1: largura * 0.51,
-        2: altura * 0.77,
-        3: largura * 0.69,
-        4: altura * 0.9,
-        val: "+",
-    },
-    {
-        1: largura * 0.73,
-        2: altura * 0.77,
-        3: largura * 0.91,
-        4: altura * 0.9,
-        val: "=",
-    },
-];
-
-const numbers_coord = [
+//---Mapeamento dos botões da calculadora
+/*
+Definição das coordenadas utilizando as devidas proporções:
+larg = 500(x)
+alt = 676(y)
+largcord = lar_area*100/x
+altcord = y*100/y
+*/
+const buttons_coord = [
     {
         1: largura * 0.086,
         2: altura * 0.2875,
@@ -156,7 +115,7 @@ const numbers_coord = [
         3: largura * 0.91,
         4: altura * 0.58,
         type: "operator",
-        val: "x",
+        val: "*",
     },
     {
         1: largura * 0.73,
@@ -171,6 +130,7 @@ const numbers_coord = [
         2: altura * 0.77,
         3: largura * 0.48,
         4: altura * 0.9,
+        type: "number",
         val: ".",
     },
     {
@@ -186,28 +146,23 @@ const numbers_coord = [
         2: altura * 0.77,
         3: largura * 0.91,
         4: altura * 0.9,
-        type: "operator",
+        type: "calculate",
         val: "=",
     },
 ];
 
-//Para cada coordenada, cria a area dos botões
-numbers_coord.forEach((e) => {
-    //Junção das coordenadas
-    var number_coord =
-        "" + e["1"] + "," + e["2"] + "," + e["3"] + "," + e["4"] + "";
-    //Cria elemento area
-    var Area = document.createElement("area");
+buttons_coord.forEach((e) => { //Para cada coordenada, cria a area dos botões
+    var button_coord =
+        "" + e["1"] + "," + e["2"] + "," + e["3"] + "," + e["4"] + ""; //Junção das coordenadas
+    var Area = document.createElement("area"); //Cria elemento area
 
     //Cria os atributos do elemento area
     var Forma = document.createAttribute("shape");
     Forma.value = "rect";
     var Cord = document.createAttribute("coords");
-    Cord.value = number_coord;
+    Cord.value = button_coord;
     var Classe = document.createAttribute("class");
-    Classe.value = "mapa__item";
-    var Href = document.createAttribute("href");
-    Href.value = "";
+    Classe.value = "map__item";
     var Value = document.createAttribute("data-value");
     Value.value = e["val"];
     var Type = document.createAttribute("data-type");
@@ -220,26 +175,72 @@ numbers_coord.forEach((e) => {
     Area.setAttributeNode(Type);
     Area.setAttributeNode(Value);
 
-    //Adição do elemento area ao elemento Mapa
-    document.getElementById("Mapa").appendChild(Area);
+    document.getElementById("Map").appendChild(Area); //Adição do elemento area ao elemento Mapa
 });
 
-//Observa interação nos botões da calculadora
-const buttons_calculator = document.querySelectorAll(".mapa__item");
+//Observa clique nos botões da calculadora
+const buttons_calculator = document.querySelectorAll(".map__item");
 buttons_calculator.forEach((button) => {
     button.addEventListener("click", () => {
-        const area_result = document.getElementById(`result`);
         const button_value = button.getAttribute("data-value");
-        if (button_value == "=") {
-            area_result.innerHTML = eval(area_result.textContent);
+        const button_type = button.getAttribute("data-type");
+        if (button_type === "calculate") {
+            calcule()
         } else {
-            area_result.innerHTML += button_value == "x" ? "*" : button_value;
+            if (button_type === 'operator') {
+                if (!has_operation) {
+                    calculation += button_value
+                    result_area.innerHTML += button_value
+                }
+                has_operation = true
+            }
+            else {
+                calculation += button_value
+                result_area.innerHTML += button_value;
+            }
         }
     });
 });
 
-const button_clear = document.getElementById(`BtnClear`);
-button_clear.addEventListener("click", () => {
-    const area_result = document.getElementById(`result`);
-    area_result.innerHTML = "";
+//Observa clique no botão limpar
+clear_button.addEventListener("click", () => {
+    clear()
 });
+
+//--- Funções
+
+function clear() {
+    result_area.innerHTML = "";
+    calculation = "";
+    has_operation = false
+}
+
+function calcule() {
+    let expression = new String(calculation);
+    let final_result = 0
+    if (expression.includes('+')) {
+        var numbers = new String(expression.split('+'))
+        n1 = numbers.substring(0, numbers.indexOf(','));
+        n2 = numbers.substring(numbers.indexOf(',') + 1, numbers.length)
+        final_result = parseFloat(n1) + parseFloat(n2);
+    } else if (expression.includes('-')) {
+        var numbers = new String(expression.split('-'))
+        n1 = numbers.substring(0, numbers.indexOf(','));
+        n2 = numbers.substring(numbers.indexOf(',') + 1, numbers.length)
+        final_result = parseFloat(n1) - parseFloat(n2);
+    } else if (expression.includes('*')) {
+        var numbers = new String(expression.split('*'))
+        n1 = numbers.substring(0, numbers.indexOf(','));
+        n2 = numbers.substring(numbers.indexOf(',') + 1, numbers.length)
+        final_result = parseFloat(n1) * parseFloat(n2);
+
+    } else if (expression.includes('/')) {
+        var numbers = new String(expression.split('/'))
+        n1 = numbers.substring(0, numbers.indexOf(','));
+        n2 = numbers.substring(numbers.indexOf(',') + 1, numbers.length)
+        final_result = parseFloat(n1) / parseFloat(n2);
+    }
+    result_area.innerHTML = final_result;
+    has_operation = false
+    calculation = final_result
+}
